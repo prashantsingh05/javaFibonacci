355e8458835c38f3a504a4d1a3de836e59db9e6f
Prashant Singh <134587691+prashantsingh05@users.noreply.github.com>
2023-08-16 20:52:07 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+// Without using Recursion \\
+
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n1=0,n2=1,n3,i,count=20;
+        System.out.print(n1 +" "+ n2); // printing 0 and 1.
+        
+        for(i=2;i<count;++i)  // loops start from 2 bcz 0 and 1 are already printed.
+        {         
+        n3=n1+n2;
+        System.out.print(" "+ n3);
+        n1=n2;
+        n2=n3;
+
+        }
+        
+    }
+}
